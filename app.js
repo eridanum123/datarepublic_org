a4c5e2285e181f62001ceefae9e39dcd2dbeedd5
eridanum123 <robinson.salazarch@gmail.com>
2017-06-15 23:59:46 -0500

    Actualizacion Historias
    Actualizacion Historias

--- app.js
+++ app.js
@@ -2,20 +2,26 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.home',
-  'myApp.countries',
-  'myApp.sidebar',
-  'myApp.network-viz',
-  'myApp.what-is',
-  'myApp.aprende',
-  'myApp.publica',
-  //'myApp.ecoView',
-  // 'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngRoute',
+    'myApp.home',
+    'myApp.countries',
+    'myApp.sidebar',
+    'myApp.network-viz',
+    'myApp.what-is',
+    'myApp.aprende',
+    'myApp.publica',
+    'myApp.objdesasoste',
+    'myApp.brechasgenero',
+    'myApp.internetdemografia',
+    'myApp.movilidadciudades',
+    'myApp.derechosexualidad',
 
-  $routeProvider.otherwise({redirectTo: '/home'});
+
+    //'myApp.ecoView',
+    // 'myApp.view2',
+    'myApp.version'
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider.otherwise({redirectTo: '/home'});
 }]);
